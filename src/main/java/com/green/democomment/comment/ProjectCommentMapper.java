76d24a6f4e0a005465145e5f6212afabf5f18eb3
KwonHyeji <kwonhyeji007@gmail.com>
2024-12-20 17:03:38 +0900

    2024-12-20 project start

--- src/main/java/com/green/democomment/comment/ProjectCommentMapper.java
+++ src/main/java/com/green/democomment/comment/ProjectCommentMapper.java
@@ -1,4 +1,7 @@
 package com.green.democomment.comment;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface ProjectCommentMapper {
 }
--- src/main/java/com/green/democomment/comment/ProjectCommentService.java
+++ src/main/java/com/green/democomment/comment/ProjectCommentService.java
@@ -1,4 +1,10 @@
 package com.green.democomment.comment;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ProjectCommentService {
+    private final ProjectCommentMapper mapper;
 }
