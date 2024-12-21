42298563e30135a0e27a19e375bc5611c0a67e25
KwonHyeji <kwonhyeji007@gmail.com>
2024-12-21 14:27:02 +0900

    2024-12-21

--- src/main/java/com/green/democomment/comment/ProjectCommentMapper.java
+++ src/main/java/com/green/democomment/comment/ProjectCommentMapper.java
@@ -1,9 +1,16 @@
 package com.green.democomment.comment;
 
+import com.green.democomment.comment.model.ProjectCommentDelReq;
+import com.green.democomment.comment.model.ProjectCommentDto;
+import com.green.democomment.comment.model.ProjectCommentGetReq;
 import com.green.democomment.comment.model.ProjectCommentPostReq;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface ProjectCommentMapper {
     void postProjectComment(ProjectCommentPostReq p);
+    List<ProjectCommentDto> getProjectCommentList(ProjectCommentGetReq p);
+    int deleteProjectComment(ProjectCommentDelReq p);
 }
